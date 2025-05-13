be38e99e2914ad8e5b688422d60749a0a9676ac1
Rob Lauer <rlauer6@comcast.net>
2025-05-13 09:13:51 -0400

    --template, --profile, documentation updates

--- lib/OrePAN2/S3.pm
+++ lib/OrePAN2/S3.pm
@@ -6,8 +6,7 @@ use warnings;
 use Amazon::Credentials;
 use Amazon::S3;
 use Archive::Tar;
-use Bedrock::Template;
-use Bedrock qw(slurp_file);
+use Template;
 use Carp;
 use CLI::Simple;
 use Data::Dumper;
@@ -15,6 +14,7 @@ use English qw(-no_match_vars);
 use File::Basename qw(basename);
 use File::Temp qw(tempfile);
 use JSON;
+use Scalar::Util qw(openhandle);
 use OrePAN2::Index;
 use YAML;
 
@@ -27,9 +27,36 @@ Readonly::Scalar our $FALSE          => 0;
 
 use parent qw(CLI::Simple);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 caller or __PACKAGE__->main();
+
+########################################################################
+sub slurp_file {
+########################################################################
+  my ( $file, $json ) = @_;
+
+  local $RS = undef;
+
+  my $content;
+
+  if ( openhandle $file) {
+    $content = <$file>;
+  }
+  else {
+    open my $fh, '<', $file
+      or die "could not open $file for reading: $OS_ERROR";
+
+    $content = <$fh>;
+
+    close $fh;
+  }
+
+  return JSON->new->decode($content)
+    if $json;
+
+  return wantarray ? split /\n/xsm, $content : $content;
+}
 
 ########################################################################
 sub fetch_config {
@@ -185,7 +212,8 @@ sub create_index {
   *{'utils::module_name'} = sub {
     my ( $self, $distribution ) = @_;
 
-    my ($module_name) = split /[\-]/xsm, basename($distribution);
+    my $module_name = basename("/$distribution");
+    $module_name =~ s/-\d+[.].*$//xsm;
 
     return $module_name;
   };
@@ -198,16 +226,16 @@ sub create_index {
     localtime => scalar localtime
   };
 
-  my $bedrock = Bedrock::Template->new(
-    { text   => $self->get_template,
-      params => $params,
-    }
-  );
+  my $text = $self->get_template;
 
-  my $index = $bedrock->parse();
+  my $template = Template->new();
 
-  return $self->send_output($index);
+  my $output = q{};
 
+  $template->process( \$text, $params, \$output )
+    or die $template->error();
+
+  return $self->send_output($output);
 }
 
 ########################################################################
@@ -301,7 +329,8 @@ sub fetch_template {
   my ($self) = @_;
 
   my $template = $self->get_template;
-  my $index    = $self->get_config->{index} // {};
+
+  my $index = $self->get_config->{index} // {};
 
   # see if the index is set in the config file...
   if ( !$template && $index->{template} ) {
@@ -356,10 +385,14 @@ sub main {
     default_options => { config_file => $DEFAULT_CONFIG, profile => 'default' },
     extra_options   => [qw(config s3 credentials template)],
     commands        => {
-      create   => \&create_index,
-      show     => \&show_orepan_index,
-      download => \&download_orepan_index,
-      upload   => \&upload_index,
+      create          => \&create_index,
+      show            => \&show_orepan_index,
+      download        => \&download_orepan_index,
+      upload          => \&upload_index,
+      'dump-template' => sub {
+        print {*STDOUT} shift->get_template;
+        return 0;
+      }
     },
   );
 
@@ -405,18 +438,18 @@ __DATA__
   <body>
     <h1>CPAN Repository</h1>
     
-<foreach --define-var="distribution" $repo.keys()->
-      <h2><var $distribution></h2>
+[% FOREACH distribution = repo.keys %]
+      <h2>[% distribution %]</h2>
 
-      <ul class="collapsable" id="<var $utils.module_name($distribution)>">
-<foreach --define-var="module" $repo.get($distribution) ->
-        <li><var $module.[0]></li>
-</foreach->
+      <ul class="collapsable" id="[% utils.module_name(distribution) %]">
+[% FOREACH module IN repo.$distribution %]
+        <li>[%  module.0 %]</li>
+[% END %]
       </ul>
-</foreach->
+[% END %]
     
     <hr>
-    <address>Generated on <var $localtime></address>
+    <address>Generated on [% localtime %] by Template::Toolkit</address>
   </body>
 </html>
 
@@ -439,6 +472,8 @@ Script for maintaining a DarkPAN mirror using S3 + CloudFront
 =item * show - Print the manifest file to STDOUT or a file.
 
 =item * upload - Upload the index.html file to the mirror's root.
+
+=item * dump-template - Outputs the default index.html template.
 
 =back
 
@@ -501,7 +536,37 @@ This section allows you to create custom template for the DarkPAN home page.
 
 =item template
 
-The name of a template file that will be parsed and uploaded as F</index.html>.
+The name of a template file that will be parsed and uploaded as
+F</index.html>. If you do not provide a template file a default
+template. The default template is a L<Template::Toolkit> style
+template. To see the default template use the C<dump-template> command
+to the F<orepan2-index> script.
+
+ orepan2-index dump-template
+
+The templating process is provided with these variables:
+
+=over 15
+
+=item utils
+
+A blessed reference to an object with one method (C<module_name>) that
+returns a verison of the module_name suitable for use as unique id.
+
+=item repo
+
+A hash of key value pairs where the key is the name of a DarkPAN
+distribution and value is an array or arrays. Each array is of the
+form:
+
+ [0] => Perl module name
+ [1] => Module version
+
+=item localtime
+
+The current time and date as a string.
+
+=back
 
 =item files
 
